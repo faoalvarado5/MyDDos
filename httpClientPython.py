dda121e7eb6ebd968f05fe1109d02e1aa2b5c881
Kevin Ledezma <kevinledezma20498@gmail.com>
2020-10-28 22:46:17 -0600

    Se agrega el codigo fuente para el cliente http en python

---
+++ httpClientPython.py
@@ -0,0 +1,19 @@
+import pycurl
+from io import BytesIO
+
+bytes_Object = BytesIO() #Captura los bytes de un archivo
+
+curl_connection = pycurl.Curl() #habilita las conexiones REST
+
+given_url = raw_input("Direccion web del recurso a descargar")
+
+curl_connection.setopt(curl_connection, given_url) #Configura la conexion curl al url ingresado
+
+curl_connection.setopt(curl_connection.WRITEDATA, bytes_Object) #Escribe los bytes codificados en utf-8 a la conexion curl
+
+curl_connection.perform() #Transfiere los datos del archivo al url destino
+
+curl_connection.close() #Cierra las conexiones REST
+
+get_content = bytes_Object.value() #Obtiene el contenido guardado en los bytes de bytes_object
+
